f3bc13b579693c2039f4fe86f299f82d4626b842
AoiTuNa <bestspark123@naver.com>
2023-10-12 16:05:49 +0900

    문자열 메시지 등 메시지 틀 구현

--- src/main/java/com/fbp/Message/Message.java
+++ src/main/java/com/fbp/Message/Message.java
@@ -1,5 +1,20 @@
 package com.fbp.Message;
 
+import lombok.Getter;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
 public class Message {
-    
+    static int count;
+    final String id;
+    final long createTime;
+
+    public Message(){
+        id = String.format("%s-%04d",getClass().getSimpleName(),count);
+        count++;
+        log.trace("message create : {}",id);
+        createTime = System.currentTimeMillis();
+    }
 }
---
+++ src/main/java/com/fbp/Message/RequestMessage.java
@@ -0,0 +1,6 @@
+package com.fbp.Message;
+
+public class RequestMessage extends Message{
+    private final String VERSION = "HTTP/1.1";
+    
+}
